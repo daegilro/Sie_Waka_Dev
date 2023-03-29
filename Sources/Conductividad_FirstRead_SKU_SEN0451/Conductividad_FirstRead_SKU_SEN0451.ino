272cbbe3e31b1c447475220d2ae395d4acd3b834
David Esteban Gil Rodríguez <daegilro@unal.edu.co>
2023-03-29 11:03:30 -0500

    update

--- Sources/Conductividad_FirstRead_SKU_SEN0451/Conductividad_FirstRead_SKU_SEN0451.ino
+++ Sources/Conductividad_FirstRead_SKU_SEN0451/Conductividad_FirstRead_SKU_SEN0451.ino
@@ -1,35 +1,40 @@
-#include <DFRobot_ECPRO.h>
+#include <DFRobot_EC.h>
+#include <EEPROM.h>
 
+#define EC_PIN 25
+//#define TE_PIN 12
 
-#define EC_PIN 10
-#define TE_PIN 12
+DFRobot_EC ec;
 
-DFRobot_ECPRO ec;
-DFRobot_ECPRO_PT1000 ecpt;
-
-uint16_t EC_Voltage, TE_Voltage;
-float Conductivity, Temp;
+float voltage,ecValue,temperature = 25;
 
 void setup()
 {
   Serial.begin(115200);
 
-  ec.setCalibration(1); //Replace the 1 with the calibrated K value if it's calibrated
-  Serial.println("Default Calibration K=" + String(ec.getCalibration()));
+  ec.begin();
+  
 }
 
 void loop()
 {
-  EC_Voltage = (uint32_t)analogRead(EC_PIN) * 5000 / 1024;
-  TE_Voltage = (uint32_t)analogRead(TE_PIN) * 5000 / 1024;
-
-  Temp = ecpt.convVoltagetoTemperature_C((float)TE_Voltage/1000);
-  Conductivity = ec.getEC_us_cm(EC_Voltage, Temp);
-
-  Serial.print("EC_Voltage: " + String(EC_Voltage) + " mV\t");
-  Serial.print("Conductivity: " + String(Conductivity) + " us/cm\t");
-  Serial.print("TE_Voltage: " + String(TE_Voltage) + " mV\t");
-  Serial.println("Temp: " + String(Temp) + " ℃");
-
-  delay(1000);
+    static unsigned long timepoint = millis();
+    if(millis()-timepoint>1000U)  //time interval: 1s
+    {
+      timepoint = millis();
+      voltage = analogRead(EC_PIN);///1024.0*5000;   // read the voltage
+      //temperature = readTemperature();          // read your temperature sensor to execute temperature compensation
+      ecValue =  ec.readEC(voltage,temperature);  // convert voltage to EC with temperature compensation
+      Serial.print("temperature:");
+      Serial.print(temperature,1);
+      Serial.print("^C  EC:");
+      Serial.print(ecValue,2);
+      Serial.println("ms/cm");
+    }
+    ec.calibration(voltage,temperature);          // calibration process by Serail CMD
 }
+
+//float readTemperature()
+//{
+  //add your code here to get the temperature from your temperature sensor
+//}
